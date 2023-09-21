f15d2d2e3a8d3445fa185b579fa9c58eade2d177
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2023-09-21 23:36:20 +0100

    Implemented sidebar and add illustrations

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 const LandingPage = () => {
   return (
-    <div>LandingPage (unprotected)</div>
+    <div>
+      LandingPage (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
